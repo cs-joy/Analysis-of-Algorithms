9f35d4edf47d4f0f4653224d024ee74b6abcb561
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-11-24 14:53:27 +0600

    new source/ ref added to the list

--- sort/heapSort/main.cpp
+++ sort/heapSort/main.cpp
@@ -1,4 +1,12 @@
 
 // Heapsort can be thought of as an improved selection sort
 
+/**
+ * prerequisite:
+ * binary tree
+ * heap data structure
+*/
+
 // ref: https://en.wikipedia.org/wiki/Heapsort
+// ref: https://www.programiz.com/dsa/heap-sort
+// ref: https://www.geeksforgeeks.org/heap-sort/
