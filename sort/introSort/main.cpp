6ec4d4ebec42e3be3401e35c02b4a0d28b5222b0
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-11-28 12:42:30 +0600

    Update main.cpp

--- sort/introSort/main.cpp
+++ sort/introSort/main.cpp
@@ -2,7 +2,16 @@
 #include <introSort/introSortSpace.hpp>
 
 using namespace std;
+using namespace implementationIntroSort;
 
+void displayResult(int arr[], int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
 
 int main()
 {
@@ -10,7 +19,8 @@ int main()
     int n = sizeof(arr) / sizeof(arr[0]);
 
     // pass the array, the pointer to the first element and the pointer to the last element
-    //
+    IntroSort(arr, arr, arr+n-1);
+    displayResult(arr, n);
 
     return(0);
 }
